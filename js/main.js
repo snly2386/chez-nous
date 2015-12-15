2644e7691c1f0c6ecbe5856371dacf5a3693a29f
Christopher Palmer <snly2386@gmail.com>
2015-12-15 12:39:46 -0800

    add family photos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   $(".home-nav-item").css('opacity', 0);
   $(".fly-logo").css('opacity', 0);
+  $(".homepage-title-text").css('opacity', 0);
   var counter = 100
+  var counterBottom = 1300
   $('.home-nav-item').each(function(){
     var that = $(this)
     setTimeout(function(){
@@ -15,12 +17,12 @@ $(document).ready(function(){
     $('.fly-logo').addClass('animated zoomIn')
   }, 1000)
 
-  // $('.home-title-text').each(function(){
-  //   var that = $(this)
-  //   setTimeout(function(){
-  //     that.animate({'opacity' : 1}, 100)
-  //     that.addClass('animated fadeInUp')
-  //   }, counter)
-  //   counterNew += 200
-  // })
+  $('.homepage-title-text').each(function(){
+    var that = $(this)
+    setTimeout(function(){
+      that.animate({'opacity' : 1}, 100)
+      that.addClass('animated fadeInUp')
+    }, counterBottom)
+    counterBottom += 300
+  })
 });
